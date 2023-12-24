e407131fb5992afe156ee68454325c0942c55c5b
JordanceE <109043515+JordanceE@users.noreply.github.com>
2023-12-25 00:04:51 +0100

    Add files via upload

---
+++ Домашна 2/Wineries/src/main/java/com/wineries/DIANSdemo/WineriesApplication.java
@@ -0,0 +1,13 @@
+package com.wineries.DIANSdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WineriesApplication.class, args);
+    }
+
+}
---
+++ Домашна 2/Wineries/src/test/java/com/wineries/DIANSdemo/WineriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wineries.DIANSdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineriesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
